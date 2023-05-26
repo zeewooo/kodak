40de4b7329e50eb5dde71454002656c91f01c61f
zeewooo <132992873+zeewooo@users.noreply.github.com>
2023-05-26 17:53:14 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let goodslistEls = document.querySelectorAll(".goodslist");
+let mebtnEl = document.querySelector(".menu");
+let menuEl = document.querySelector("#menu");
+
+/**
+ * 1. forEach
+ * 2. addEventListener
+ */
+
+goodslistEls.forEach((element,index)=>{
+    element.addEventListener("mouseover",()=>{
+        element.style.border="1px solid #333333";
+    })
+    element.addEventListener("mouseout",()=>{
+        element.style.border="1px solid #eee";
+    })
+})
+
+mebtnEl.addEventListener("click",()=>{
+    menuEl.style.transform="translateX(0%)";
+})
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true
+    },
+    autoplay:{
+        delay:4000
+    }
+   
+});
+
+
+
+
+
+
+
+
+
